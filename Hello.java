2926ce1c71b5e7626c5e7f0ec680a09cdbd64437
chenzhuo1791 <37822707+chenzhuo1791@users.noreply.github.com>
2018-03-27 18:06:48 +0800

    Change format
    Change format for beauty look !

--- Hello.java
+++ Hello.java
@@ -1,8 +1,10 @@
 
 Class Hello {
-    public static void main(String[] args){
-          System.out.println("## Hello  world ! &&");
+    
+    public static void main(String[] args){   
           int a = 5 ;
-          System.out.println("## result : " + char(a));
+          System.out.println("#result : " + char(a));
+          System.out.println("## Hello  world ! &&");
     }
+    
  }
