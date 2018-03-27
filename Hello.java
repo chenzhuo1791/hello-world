b9a82140afeb9dd45dd773060395455c101086c2
chenzhuo1791 <37822707+chenzhuo1791@users.noreply.github.com>
2018-03-27 18:01:02 +0800

    Update Hello.java file
    save original change and add value of char function !

--- Hello.java
+++ Hello.java
@@ -1,6 +1,8 @@
 
 Class Hello {
-  public static void main(String[] args){
+    public static void main(String[] args){
           System.out.println("## Hello  world ! &&");
+          int a = 5 ;
+          System.out.println("## result : " + char(a));
     }
  }
